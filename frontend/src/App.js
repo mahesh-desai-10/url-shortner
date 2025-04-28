87ca6d37c0bf2dda31a03723a17a39e4a9352466
mahesh-desai-10 <mahesh.desai.work@gmail.com>
2025-04-28 09:13:49 +0530

    change API_BASE_URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE_URL = "http://localhost:5000";
+// const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 function App() {
   const [originalUrl, setOriginalUrl] = useState("");
